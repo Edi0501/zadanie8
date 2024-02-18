b37d69aaed01d81339aefc0548c515f5f2219cf7
Edi0501 <edytanowakowska05@gmail.com>
2024-02-18 08:52:39 +0100

    Add project files.

---
+++ zadanie8/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace zadanie8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj wyniki testu z matematyki, chemii i fizyki");
+            Console.WriteLine("Matematyka:");
+            int a = 25;
+            int b = 63;
+            int c = 79;
+
+            Int32.TryParse(Console.ReadLine(), out a);
+            Console.WriteLine("Fizyka:");
+            Int32.TryParse(Console.ReadLine(), out b);
+            Console.WriteLine("Chemia:");
+            Int32.TryParse(Console.ReadLine(), out c);
+            if (a + b + c >= 180)
+            {
+                Console.WriteLine("Kandydat dopuszczony do rekrutacji");
+            }
+            else if (a + b >= 150)
+            {
+                Console.WriteLine("Kandydat dopuszczony do rekrutacji");
+            }
+            else if (a + c >= 150)
+            {
+                Console.WriteLine("Kandydat dopuszczony do rekrutacji");
+            }
+
+            else
+            {
+                Console.WriteLine("Przykro mi, masz za mało punktów.");
+            }
+        }
+    }
+}
+
